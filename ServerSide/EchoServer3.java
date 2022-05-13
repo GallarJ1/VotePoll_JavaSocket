0261b982469dfb2f6b45f5231c8d715974846035
GallarJ1 <87103825+GallarJ1@users.noreply.github.com>
2022-05-13 18:11:30 -0500

    Add files via upload

---
+++ ServerSide/EchoServer3.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.net.*;
+
+/**
+ * This module contains the application logic of an echo server
+ * which uses a stream-mode socket for interprocess communication.
+ * Unlike EchoServer2, this server services clients concurrently.
+ * A command-line argument is required to specify the server port.
+ * @author M. L. Liu
+ */
+
+public class EchoServer3 {
+   static int yesCount = 0;
+   static int noCount = 0;
+   static int dontCareCount = 0;
+   public static void main(String[] args) {
+      int serverPort = 7;    // default port
+      String message;
+      if (args.length == 1 )
+         serverPort = Integer.parseInt(args[0]);       
+      try {
+         // instantiates a stream socket for accepting
+         //   connections
+       ServerSocket myConnectionSocket = 
+            new ServerSocket(serverPort); 
+/**/     System.out.println("Echo server ready.");  
+         while (true) {  // forever loop
+            // wait to accept a connection 
+/**/        System.out.println("Waiting for a connection.");
+            MyStreamSocket myDataSocket = new MyStreamSocket
+                (myConnectionSocket.accept( ));
+/**/        System.out.println("connection accepted");
+            // Start a thread to handle this client's sesson
+            Thread theThread = 
+               new Thread(new EchoServerThread(myDataSocket));
+            theThread.start();
+            // and go on to the next client
+            } //end while forever
+       } // end try
+     catch (Exception ex) {
+          ex.printStackTrace( );
+     } // end catch
+   } //end main
+   static public synchronized void incrementYes()
+   {
+     yesCount++;
+   }
+   static public synchronized void incrementNo()
+   {
+     noCount++;
+   }
+   static public synchronized void incrementDontCare()
+   {
+     dontCareCount++;
+   }  
+   static public int getYesCount()
+   {
+     return yesCount;
+   } 
+   static public int getNoCount()
+   {
+     return noCount;
+   }
+   static public int getDontCareCount()
+   {
+     return dontCareCount;
+   }      
+} // end class
---
+++ ServerSide/EchoServerThread.java
@@ -0,0 +1,82 @@
+import java.io.*;
+/**
+ * This module is to be used with a concurrent Echo server.
+ * Its run method carries out the logic of a client session.
+ * @author M. L. Liu
+ */
+
+class EchoServerThread implements Runnable {
+   static final String endMessage = ".";
+   MyStreamSocket myDataSocket;
+   EchoServerThread(MyStreamSocket myDataSocket) {
+      this.myDataSocket = myDataSocket;
+   }
+   public void run( ) {
+      boolean done = false;
+      String message;
+      String error = "Sorry incorrect input please try again";
+      try {
+        while(true)
+        {
+          message = myDataSocket.receiveMessage();     //receive
+          if((message.trim()).equals("I want to vote"))
+          {
+            break;
+          }
+          else
+          {
+           message = error;
+           myDataSocket.sendMessage(message);     //optional send
+          }
+        }
+        while(true)
+        {
+          message = "Vote(1) yes, (2) no, or (3) don't care" ;
+          myDataSocket.sendMessage(message);       //send 
+          message = myDataSocket.receiveMessage();  //recieve
+          if((message.trim()).equals("1"))
+          {
+            EchoServer3.incrementYes();
+            break;
+          }
+          else if((message.trim()).equals("2"))
+          {
+            EchoServer3.incrementNo();
+            break;
+          }
+          else if((message.trim()).equals("3"))
+          {
+            EchoServer3.incrementDontCare();
+            break;
+          }
+          else
+          {
+            message = error;
+            myDataSocket.sendMessage(message);   //optional send
+          }
+        }      
+         while (!done) 
+         {
+             message = ("Vote recorded, Press any number to see result or . to exit") ;
+             myDataSocket.sendMessage(message);   // send 
+             message = myDataSocket.receiveMessage( );  //receive          
+             if ((message.trim()).equals (endMessage)){
+                //Session over; close the data socket.
+/**/            System.out.println("Session over.");
+                myDataSocket.close( );
+                done = true;
+             } //end if
+             else {
+                // Now send the echo to the requesto
+                message = ("Scores are: " + EchoServer3.getYesCount() + " Yes, " +
+                EchoServer3.getNoCount() + " No, " + EchoServer3.getDontCareCount()
+                + " Don't Care");             
+                myDataSocket.sendMessage(message); //send 
+             } //end else
+          } //end while !done
+        }// end try
+        catch (Exception ex) {
+           System.out.println("Exception caught in thread: " + ex);
+        } // end catch
+   } //end run
+} //end class 
